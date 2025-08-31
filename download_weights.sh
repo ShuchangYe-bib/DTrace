cf8689efba6fa1e98309c32082e1c3e31a27d778
ShuchangYe-bib <yeshuchang.bib@gmail.com>
2025-08-31 11:59:47 +1000

    script to download weights

---
+++ download_weights.sh
@@ -0,0 +1,3 @@
+mkdir weights
+wget -P ./weights/ https://dl.fbaipublicfiles.com/mae/pretrain/mae_pretrain_vit_base.pth
+wget -P ./weights/ https://dl.fbaipublicfiles.com/mae/visualize/mae_visualize_vit_base.pth
